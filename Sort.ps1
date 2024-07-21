813b98e833ad1b289454c5acb0f9542d0d1975b2
DanDeanDean <dpd5518@psu.edu>
2024-07-21 09:18:09 -0400

    Add ParseArgs

--- Sort.ps1
+++ Sort.ps1
@@ -1,25 +1,3 @@
-if ($args.count -ne 3) {
-    throw "Usage: Sort.ps1 <File> <Type> <Direction>"
-}
-$filepath = $args[0]
-$type = $args[1].ToString().ToLower()
-$direction = $args[2].ToString().ToLower()
-# TODO: look into using param for this section
-if (-not (Test-Path $filepath)) {
-    throw "The path: $filepath does not exist."
-}
-if ( -not( @('alpha','both','numeric','a','b','n') -contains $type)) {
-    throw "Please choose '(a)lpha', '(n)umeric', or '(b)oth'. Invalid type: $type." 
-}
-switch ($type[0]) {
-    "a" { $filter = "string" }
-    "n" { $filter = "double" }
-    default { $filter = "object" }
-}
-if ( -not( @('ascending','descending','a','d') -contains $direction)) {
-    throw "Please choose '(a)scending' '(d)escending'. Invalid direction: $direction." 
-}
-
 function SmartCast($item) {
     try {
         $item = [double] $item
@@ -33,9 +11,33 @@ function SmartCast($item) {
 
 function FilterByType($array, $type){
     $out = ($array | Where-Object { $_ -is [type]$type })
-    #Write-Host $out
     return $out
 }
+
+function ParseArgs($argv) {
+    if ($args.count -ne 3) {
+        throw "Usage: Sort.ps1 <File> <Type> <Direction>"
+    }
+    $script:filepath = $argv[0]
+    $script:type = $argv[1].ToString().ToLower()
+    $script:direction = $argv[2].ToString().ToLower()
+    if (-not (Test-Path $filepath)) {
+        throw "The path: $filepath does not exist."
+    }
+    if ( -not( @('alpha', 'both', 'numeric', 'a', 'b', 'n') -contains $type)) {
+        throw "Please choose '(a)lpha', '(n)umeric', or '(b)oth'. Invalid type: $type." 
+    }
+    switch ($type[0]) {
+        "a" { $script:filter = "string" }
+        "n" { $script:filter = "double" }
+        default { $script:filter = "object" }
+    }
+    if ( -not( @('ascending', 'descending', 'a', 'd') -contains $direction)) {
+        throw "Please choose '(a)scending' '(d)escending'. Invalid direction: $direction." 
+    }
+}
+
+ParseArgs $args
 
 
 #TODO: Fix this whole block, should probably be a one-liner
